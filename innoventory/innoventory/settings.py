177a298311d5966a4246fd6f147fdd9021968bb9
Michaela Estrera <L13X16W18@LabHCI.net>
2025-10-29 17:23:16 +0800

    setup(render): changed params for local and online db

--- innoventory/innoventory/settings.py
+++ innoventory/innoventory/settings.py
@@ -84,14 +84,21 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
 ]
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
-        env="DATABASE_URL",
-        conn_max_age=600,  # persistent connections
-        ssl_require=True  # enforce SSL
-    )
-}
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # DATABASES = {
 #     'default': {
