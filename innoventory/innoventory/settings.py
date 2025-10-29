ed9dbe4dee290a6fd06400bd2695b6b7124cc063
Michaela Estrera <L13X16W18@LabHCI.net>
2025-10-29 17:28:43 +0800

    setup(render): added domain as allowed host

--- innoventory/innoventory/settings.py
+++ innoventory/innoventory/settings.py
@@ -21,7 +21,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["innoventory.onrender.com"]
 
 # Application definition
 
