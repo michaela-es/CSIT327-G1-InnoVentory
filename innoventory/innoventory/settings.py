f56fa0cad8571229cf9c803ee57bacbb627b1210
Michaela Estrera <L13X16W18@LabHCI.net>
2025-10-29 16:20:24 +0800

    setup: deploy on render

--- innoventory/innoventory/settings.py
+++ innoventory/innoventory/settings.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
-from pathlib import Path
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+DEBUG = os.getenv("DEBUG") == "True"
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALLOWED_HOSTS = os.getenv['localhost','127.0.0.1','.onrender.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ji$&@527q%y)5^xn8df3v&28g9c@iv9nhzia1uf$4m^^r$ltpo'
+# SECURITY WARNING: keep the secret key used in production secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
