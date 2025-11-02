eb3f7efbefbf944821427b524018e0a87b78479f
michaela-es <michaela.destrera@gmail.com>
2025-11-02 17:55:40 +0800

    fix(stocks): added check for null products in recent stock transactions page

--- innoventory/stocks/views.py
+++ innoventory/stocks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Stocks
+
 def stock_management(request):
-    stocks = Stocks.objects.order_by('-date')[:5]
+    stocks = Stocks.objects.filter(product__isnull=False).order_by('-date')[:5]
     return render(request, 'stocks/stock_management.html', {'stocks': stocks})
