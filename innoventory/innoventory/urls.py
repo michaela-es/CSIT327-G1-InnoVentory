226d746284b3ef47a8c51ecb1de2341d43837449
michaela-es <michaela.destrera@gmail.com>
2025-11-02 18:31:48 +0800

    fix(stocks): removed stocks from urls.py

--- innoventory/innoventory/urls.py
+++ innoventory/innoventory/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
-    path('stocks/', include('stocks.urls')),
     path('sales/', include('sales.urls'))
 ]
