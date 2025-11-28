68fa1d0e31a3771fcf49b665b9a05ca724a3ae18
michaela-es <michaela.destrera@gmail.com>
2025-11-28 19:31:42 +0800

    fix(settings): fixed incorrect import

--- innoventory/innoventory/views.py
+++ innoventory/innoventory/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from accounts.forms import UserEditForm, ThresholdForm
+from accounts.forms import UserEditForm
+from products.forms ThresholdForm
 from sales.models import InventorySettings
 
 @login_required
